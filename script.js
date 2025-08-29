44a569ee0bfb70dbb55d9e5b31cef8d1cb7c2833
Umme Hani <uh.snigdha@gmail.com>
2025-08-29 20:43:32 +0600

    copy clipboard and alert added

--- script.js
+++ script.js
@@ -10,12 +10,22 @@ document.getElementById("heart1").addEventListener('click', (e) => {
 
 document.getElementById("copy1").addEventListener('click', (e) => {
     // e.preventDefault();
-    let copy = document.getElementById("count-copy");
-    let numCopy = parseInt(copy.innerText);
     
-    numCopy++;
-
-    copy.innerText = numCopy;
-
     // copy to clipboard;
+
+    navigator.clipboard.writeText(document.getElementById("hotline1").innerText)
+        .then(result => {
+            alert("Copy successful!");
+            
+            let copy = document.getElementById("count-copy");
+            let numCopy = parseInt(copy.innerText);
+    
+            numCopy++;
+
+            copy.innerText = numCopy;
+        }) 
+        .catch(reason => {
+            alert("Copy is not successful!");
+        })
+
 })
