fd10b7342f662b2cda26a88c205ffd123f8ecccf
Umme Hani <uh.snigdha@gmail.com>
2025-08-29 20:14:24 +0600

    heart count event

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('test');
+document.getElementById("heart1").addEventListener('click', (e) => {
+    // e.preventDefault();
+    let heart = document.getElementById("heart-count");
+    let numHeart = parseInt(heart.innerText);
+    
+    numHeart++;
+
+    heart.innerText = numHeart;
+})
